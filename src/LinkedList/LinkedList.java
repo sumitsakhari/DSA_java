d7c2d9e56fc7f978e84cdb49dea329dd42d852f5
sumitsakhari <sumitsakhari2004@gmail.com>
2026-08-13 00:38:11 +0530

    implemented Singly Linked List

--- src/LinkedList/LinkedList.java
+++ src/LinkedList/LinkedList.java
@@ -12,15 +12,15 @@ public class LinkedList {
         return head;
     }
 
-    public void insertAtBeginning(int data) {
-        ListNode node = new ListNode(data);
+    public void insertAtBeginning(ListNode node) {
+//        ListNode node = new ListNode(data);
         node.next = head;
         head = node;
         length++;
     }
 
-    public void insertAtEnd(int data) {
-        ListNode node = new ListNode(data);
+    public void insertAtEnd(ListNode node) {
+//        ListNode node = new ListNode(data);
         if(head==null) {
             head = node;
         }else {
@@ -58,8 +58,103 @@ public class LinkedList {
         length++;
     }
 
-    public void removeFromBeginning() {
-
+    public ListNode removeFromBeginning() {
+        ListNode node = head;
+        if(node!=null) {
+            head = node.next;
+            node.next = null;
+        }
+        if(head!=null) length--;
+        return node;
     }
-    
+
+    public ListNode removeFromEnd() {
+        if(head==null) return null;
+        ListNode p = head,q = null,node = p.next;
+        if(node==null) {
+            head = null;
+            length--;
+            return p;
+        }
+        while(node!=null) {
+            q = p;
+            p = node;
+            node = p.next;
+        }
+        q.next = null;
+        length--;
+        return p;
+    }
+
+    public void removeMatched(ListNode node) {
+        if(head==null) return;
+        if(node.equals(head)) {
+            head = head.next;
+            length--;
+            return;
+        }
+        ListNode p = head,q = p.next;
+        while(q!=null) {
+            if (node.equals(q)) {
+                p.next = q.next;
+                length--;
+                return;
+            }
+            p = q;
+            q = p.next;
+        }
+    }
+
+    public void removeAtIndex(int index) {
+        if(index<0) index = 0;
+        if(index>length) index = length - 1;
+
+        if(head==null) return;
+        if(index==0) {
+            head = head.next;
+        }else {
+            ListNode temp = head;
+            for(int i=1;i<index;i++) {
+                temp = temp.next;
+            }
+            temp.next = temp.next.next;
+        }
+        length--;
+    }
+
+    public String displayList() {
+        String res = "[";
+        if(head==null) return res+"]";
+        res = res + head.data;
+        ListNode temp = head.next;
+        while(temp!=null) {
+            res = res + "," + temp.data;
+            temp = temp.next;
+        }
+
+        return res + "]";
+    }
+
+    public int length() {
+        return length;
+    }
+
+    public int getIndex(int data) {
+        int index = 0;
+        ListNode temp = head;
+        while(temp!=null) {
+            if(temp.data==data) {
+                return index;
+            }
+            index++;
+            temp = temp.next;
+        }
+
+        return -1;
+    }
+
+    public void clearList() {
+        head = null;
+        length = 0;
+    }
 }
--- src/Main.java
+++ src/Main.java
@@ -8,12 +8,36 @@ public class Main {
         ListNode n1 = new ListNode(10);
         ListNode n2 = new ListNode(20);
         ListNode n3= new ListNode(30);
+        ListNode n4= new ListNode(40);
+        ListNode n5= new ListNode(50);
+        ListNode n6= new ListNode(60);
 
-        n1.next = n2;
-        n2.next = n3;
+        LinkedList list = new LinkedList();
+        System.out.println(list.length());
+        list.insertAtEnd(n2);
+        System.out.println(list.displayList());
+        list.insertAtBeginning(n1);
+        System.out.println(list.displayList());
+        list.insertAtEnd(n3);
+        list.insertAtEnd(n4);
+        list.insertAtEnd(n5);
+        list.insertAtEnd(n6);
+        System.out.println(list.displayList());
+        list.insertAtMiddle(2,25);
+        System.out.println(list.displayList());
+        System.out.println(list.length());
+        System.out.println(list.removeFromBeginning());
+        System.out.println(list.removeFromEnd());
+        System.out.println(list.displayList());
+        list.removeMatched(n3);
+        System.out.println(list.displayList());
+        list.removeAtIndex(2);
+        System.out.println(list.displayList());
+        System.out.println(list.length());
+        System.out.println(list.getIndex(25));
+        list.clearList();
+        System.out.println(list.displayList());
+        System.out.println(list.length());
 
-        System.out.println("Data: " + n1.data +"  "+ "NextNode: " + n1.next);
-        System.out.println("Data: " + n2.data +"  "+ "NextNode: " + n2.next);
-        System.out.println("Data: " + n3.data +"  "+ "NextNode: " + n3.next);
     }
 }
